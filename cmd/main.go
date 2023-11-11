3ad07ed8555d4835098fa7ebe01f46775a073031
Matheus Dussin Bampi <matheusbampi@gmail.com>
2023-11-11 09:47:10 -0300

    feat: add testing feature, returning avg messages count

--- cmd/main.go
+++ cmd/main.go
@@ -10,14 +10,18 @@ import (
 )
 
 func main() {
-
 	numProcesses := getNumProcesses()
 
-	ring := hs.NewRing(numProcesses)
+	// read flag to determine whether to run the algorithm or the test
+	if len(os.Args) > 2 {
+		if os.Args[2] == "test" {
+			avgMessages := testAlgorithm(numProcesses)
+			fmt.Printf("Average number of messages: %d\n", avgMessages)
+			return
+		}
+	}
 
-	ring.Run()
-
-	fmt.Printf("Number of messages: %d\n", stats.NumMessages.Get())
+	runAlgorithm(numProcesses)
 }
 
 // getNumProcesses returns the number of processes to create,
@@ -33,4 +37,29 @@ func getNumProcesses() int {
 	}
 
 	return numProcesses
+}
+
+// runAlgorithm runs the algorithm once and prints the number of messages.
+func runAlgorithm(numProcesses int) {
+	ring := hs.NewRing(numProcesses)
+
+	ring.Run()
+
+	fmt.Printf("Number of messages: %d\n", stats.NumMessages.Get())
+}
+
+// testAlgorithm runs the algorithm 5 times and returns the average number of messages.
+func testAlgorithm(numProcesses int) int {
+	numTests := 5
+	totalMessages := 0
+
+	for i := 0; i < numTests; i++ {
+		ring := hs.NewRing(numProcesses)
+		ring.Run()
+
+		totalMessages += stats.NumMessages.Get()
+		stats.NumMessages.Reset()
+	}
+
+	return totalMessages / numTests
 }
